35397d68bb2387f72412df409df7d6400c169e6e
Qihe Bian <ufosky@gmail.com>
2014-09-21 16:52:22 +0800

    Modify page.

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,5 @@
-require("cloud/app.js");
+// require("cloud/app.js");
+require("cloud/index.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
